fffeb295a95f4ec502bef948cd47e906663c0569
Don Pinkus <don.pinkus@gmail.com>
2014-09-09 20:18:30 -0700

    If you are the owner of timeline, or the owner of an event, you may edit it.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -45,7 +45,7 @@ class EventsController < ApplicationController
   def update
     respond_to do |format|
       if @event.update(event_params)
-        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
+        format.html { redirect_to @event.timeline, notice: 'Event was successfully updated.' }
         format.json { render :show, status: :ok, location: @event }
       else
         format.html { render :edit }
@@ -59,7 +59,7 @@ class EventsController < ApplicationController
   def destroy
     @event.destroy
     respond_to do |format|
-      format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
+      format.html { redirect_to @event.timeline, notice: 'Event was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
